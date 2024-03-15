2963a8778db6ffcde0be71b9b9d178470b58c3f3
bs240 <Ilya_Zhorn@yahoo.com>
2024-03-15 21:47:53 +0200

    This is the second console.js version

--- hello-nodejs/console.js
+++ hello-nodejs/console.js
@@ -1,4 +1,43 @@
-console.log(2+2)
-console.log(4-2)
-console.log(4*4)
-console.log(
+console.log(9%8);
+console.log(16%8);
+console.log(2**3);
+console.log(2>1);
+console.log(1==2);
+console.log(100=="100");
+console.log(100==="100");
+console.log(1!="1");
+console.log(2!=="2");
+var currentTime=10;
+console.log(currentTime>9 && currentTime<17);
+currentTime=7;
+console.log(currentTime<9 || currentTime>17);
+var petHungry=true;
+console.log("Feeding the pet");
+console.log("Pet is hungry:",!petHungry);
+console.log(petHungry);
+//using the loginal && operator
+var score=8
+console.log("Mid-term skills:",score>0 && score<10);
+//using the logical || operator
+var timeRemaining=0;
+var energy=10;
+console.log("Game over:",timeRemaining==0 || energy==0);
+//use += operator to accumulate values in variable
+var counter=0;
+counter +=5
+counter +=3
+console.log(counter);
+//concatinating numbers and string using + operator
+var now="Now in";
+var three=3;
+var d="D!";
+console.log(now + " "+three+d);
+console.log(365 + " days");
+console.log(12 + " months");
+console.log("1972");
+console.log(1972);
+console.log("Ilya Zhornitskiy(1972)");
+console.log("Ilya");console.log("Zhornitskiy");console.log("1972");
+console.log("Ilya "+"Zhornitskiy "+"1972");
+
+
